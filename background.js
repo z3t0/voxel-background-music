8e7904b01203f676c446cea889519f7dd5987797
Rafi Khan <zetoslab@gmail.com>
2015-04-11 18:29:10 -0600

    Added disable function

--- background.js
+++ background.js
@@ -11,24 +11,27 @@ module.exports.pluginInfo = {
 };
 
 function BackgroundMusicPlugin(game, opts) {
+
   this.game = game;
   this.artPacks = this.game.materials.artPacks;
+
 
   this.enable();
 }
 
 BackgroundMusicPlugin.prototype.enable = function() {
   var self = this;
-  this.game.materials.artPacks.on('loadedAll', this.loaded = function(){
-    this.voxelSfx = game.plugins.get('voxel-sfx');
-    if(!this.voxelSfx) throw new Error('voxel-background-music requires voxel-sfx')
-    this.voxelSfx.play('random/background', true);
+  self.voxelSfx = game.plugins.get('voxel-sfx');
+  if(!this.voxelSfx) throw new Error('voxel-background-music requires voxel-sfx')
+
+  self.game.materials.artPacks.on('loadedAll', this.loaded = function(){
+    self.voxelSfx.play('random/background', true);
   });
-  // Register listeners
   console.log("PLAYING BACKGROUND")
 
 }
 
 BackgroundMusicPlugin.prototype.disable = function() {
-
+  var self = this;
+  self.voxelSfx.pause('random/background');
 }
